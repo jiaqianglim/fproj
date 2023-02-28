5b5efba51140f5505a8b72c0aeb42c059fd81a75
jiaqianglim <jqlimjiaqiang@gmail.com>
2023-02-28 20:48:28 +0800

    first commit

---
+++ controlspringboot/README.md
@@ -0,0 +1,2 @@
+Spring app used for testing, dependency and versioning.
+This app has no functionality.
---
+++ controlspringboot/src/main/java/com/togetherness/controlspringboot/ControlspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.togetherness.controlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlspringbootApplication.class, args);
+	}
+
+}
